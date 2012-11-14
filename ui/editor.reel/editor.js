f40ef486762feb4b64be211846889d626b8dabb5
François Frisch <francoisfrisch@gmail.com>
2012-11-14 14:07:58 -0800

    Adding basic building blocks

---
+++ ui/editor.reel/editor.js
@@ -0,0 +1,16 @@
+/**
+    @module "ui/editor.reel"
+    @requires montage
+    @requires montage/ui/component
+*/
+var Montage = require("montage").Montage,
+    Component = require("montage/ui/component").Component;
+
+/**
+    Description TODO
+    @class module:"ui/editor.reel".Editor
+    @extends module:montage/ui/component.Component
+*/
+exports.Editor = Montage.create(Component, /** @lends module:"ui/editor.reel".Editor# */ {
+
+});
