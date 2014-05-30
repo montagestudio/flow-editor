cc20c2efa0052c5589586a732e48dd21d5328735
Thibault Zanini <thibaultzanini@gmail.com>
2014-05-29 18:37:19 -0700

    Add a comment to explain why we are adding just listeners on the first viewport.

--- core/viewport-event-manager.js
+++ core/viewport-event-manager.js
@@ -21,6 +21,8 @@ exports.ViewPortEventManager = Target.specialize({
                     updated = function (event) { self.handleViewportChange(event);};
 
                 viewPorts.forEach(function (viewPort, index) {
+
+                    // viewports share the same scene, so just need to add listeners on the first viewport
                     if (index === 0) {
                         viewPort.scene._data.addEventListener("vectorChange", updated, false);
                         viewPort.scene._data.addEventListener("bezierCurveChange", updated, false);
