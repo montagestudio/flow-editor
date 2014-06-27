2b7c5d3e7b0a1d4738356f32fa6330a7f4a0a54e
Thibault Zanini <thibaultzanini@gmail.com>
2014-06-27 13:55:30 -0700

    Add RemoveCanvasFlowKnot into the Scene Object

--- ui/flow-knot-inspector.reel/flow-knot-inspector.js
+++ ui/flow-knot-inspector.reel/flow-knot-inspector.js
@@ -128,22 +128,8 @@ exports.FlowKnotInspector = Component.specialize( /** @lends module:"ui/flow-kno
 
     handleDeleteAction: {
         value: function () {
-            var spline = this.knot.parent,
-                i;
-
-            if (spline._data.knotsLength <= 2) {
-                this.scene.removeCanvasFlowSpline(spline);
-            } else {
-                i = 0;
-                while ((i < spline.children.length) && (spline.children[i] !== this.knot)) {
-                    i++;
-                }
-                if (i < spline.children.length) {
-                    spline._data.removeKnot(spline._data.getKnot(i));
-                    this.knot.delete();
-                    this.dispatchEventNamed("flowPropertyChangeSet", true, true);
-                }
-            }
+            this.scene.removeCanvasFlowKnot(this.knot);
+            this.dispatchEventNamed("flowPropertyChangeSet", true, true);
         }
     }
 
